452ebb2a1f80385c6830810a3a29fda39b1c54c6
bala-amypo <bala@amypo.in>
2025-12-18 09:08:41 +0000

    User commit

--- src/main/java/com/example/demo/service/AuthService.java
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -1,7 +1,11 @@
 package com.example.demo.service;
+
+import com.example.demo.model.AuthRequest;
+import com.example.demo.model.AuthResponse;
+
 public interface AuthService {
 
+    AuthResponse register(AuthRequest request);
 
-
-    
+    AuthResponse login(AuthRequest request);
 }
---
+++ src/main/java/com/example/demo/service/AuthServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import com.example.demo.model.AuthRequest;
+import com.example.demo.model.AuthResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    @Override
+    public AuthResponse register(AuthRequest request) {
+        return new AuthResponse("Registered successfully");
+    }
+
+    @Override
+    public AuthResponse login(AuthRequest request) {
+        return new AuthResponse("Login successful");
+    }
+}
