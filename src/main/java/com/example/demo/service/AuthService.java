5ab1c3025049b577e6d0255fbb4afeeaa38e5368
bala-amypo <bala@amypo.in>
2025-12-21 16:16:09 +0000

    User commit

--- src/main/java/com/example/demo/service/AuthService.java
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -1,7 +1,7 @@
 package com.example.demo.service;
 
-import com.example.demo.model.AuthRequest;
-import com.example.demo.model.AuthResponse;
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.dto.AuthResponse;
 
 public interface AuthService {
     AuthResponse register(AuthRequest request);
--- src/main/java/com/example/demo/service/AuthServiceImpl.java
+++ src/main/java/com/example/demo/service/AuthServiceImpl.java
@@ -1,7 +1,7 @@
 package com.example.demo.service;
 
-import com.example.demo.model.AuthRequest;
-import com.example.demo.model.AuthResponse;
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.dto.AuthResponse;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,11 +9,11 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public AuthResponse register(AuthRequest request) {
-        return new AuthResponse("User registered successfully");
+        return new AuthResponse(null, request.getEmail(), "INVESTOR");
     }
 
     @Override
     public AuthResponse login(AuthRequest request) {
-        return new AuthResponse("Login successful");
+        return new AuthResponse("jwt-token", request.getEmail(), "INVESTOR");
     }
 }
