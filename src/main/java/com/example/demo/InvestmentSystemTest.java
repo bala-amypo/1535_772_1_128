e0de47fe961d35a17681e7f84f02d2e826ff105a
bala-amypo <bala@amypo.in>
2025-12-25 17:33:16 +0000

    User commit

---
+++ src/main/java/com/example/demo/InvestmentSystemTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentSystemTest {
+
+    @Test
+    void contextLoads() {
+        // app starts successfully
+    }
+}
