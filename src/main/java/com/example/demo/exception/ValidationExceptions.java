6a24eee77b6b3b2926050d0df91f2eef36cde657
bala-amypo <bala@amypo.in>
2025-12-26 08:30:21 +0000

    User commit

--- src/main/java/com/example/demo/exception/ValidationExceptions.java
+++
@@ -1,3 +0,0 @@
-throw new IllegalArgumentException("between 0 and 100");
-throw new IllegalArgumentException("must be > 0");
-throw new IllegalArgumentException("currentPercentage > targetPercentage");
