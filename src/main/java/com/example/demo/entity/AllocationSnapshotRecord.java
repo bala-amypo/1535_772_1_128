dc3969bc0262b4facd3d5e0a1955783d7893c8f3
bala-amypo <bala@amypo.in>
2025-12-26 03:57:01 +0000

    User commit

--- src/main/java/com/example/demo/entity/AllocationSnapshotRecord.java
+++ src/main/java/com/example/demo/entity/AllocationSnapshotRecord.java
@@ -1,44 +1,5 @@
-package com.example.demo.entity;
-
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "allocation_snapshot_records")
-public class AllocationSnapshotRecord {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private Long investorId;
-    private String assetClass;
-    private Double percentage;
-
-    public Long getId() {
-        return id;
-    }
-
-    public Long getInvestorId() {
-        return investorId;
-    }
-
-    public void setInvestorId(Long investorId) {
-        this.investorId = investorId;
-    }
-
-    public String getAssetClass() {
-        return assetClass;
-    }
-
-    public void setAssetClass(String assetClass) {
-        this.assetClass = assetClass;
-    }
-
-    public Double getPercentage() {
-        return percentage;
-    }
-
-    public void setPercentage(Double percentage) {
-        this.percentage = percentage;
-    }
+@GetMapping("/investor/{investorId}")
+public List<AllocationSnapshotRecord> getByInvestorId(
+        @PathVariable String investorId) {
+    return service.getByInvestorId(investorId);
 }
