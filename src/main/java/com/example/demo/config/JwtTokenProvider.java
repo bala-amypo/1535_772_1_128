a647e81d9d80bff4fba12a2f336e7737ec405e8e
bala-amypo <bala@amypo.in>
2025-12-26 09:21:59 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtTokenProvider.java
+++ src/main/java/com/example/demo/config/JwtTokenProvider.java
@@ -10,7 +10,7 @@ import java.util.Date;
 @Component
 public class JwtTokenProvider {
 
-    private final String SECRET_KEY = "mysecretkeymysecretkeymysecretk2";
+private final String SECRET_KEY ="mysecretkeymysecretkeymysecretkeymysecretkey123456";
     private final long EXPIRATION = 1000 * 60 * 60; // 1 hour
 
     private Key getSigningKey() {
