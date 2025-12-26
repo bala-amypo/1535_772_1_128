2c9190b05a41d5834ed436f2582aeeeb87b65a8b
bala-amypo <bala@amypo.in>
2025-12-26 15:54:02 +0000

    User commit

---
+++ src/main/java/com/example/demo/security/CustomUserDetails.java
@@ -0,0 +1,56 @@
+package com.example.demo.security;
+
+import com.example.demo.entity.UserAccount;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserDetails implements UserDetails {
+    private final UserAccount userAccount;
+
+    public CustomUserDetails(UserAccount userAccount) {
+        this.userAccount = userAccount;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + userAccount.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return userAccount.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return userAccount.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return userAccount.getActive();
+    }
+
+    public UserAccount getUserAccount() {
+        return userAccount;
+    }
+}
