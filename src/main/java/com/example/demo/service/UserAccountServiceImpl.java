f0db3c1ca786434288079d9cbdfb791a3ef9a499
bala-amypo <bala@amypo.in>
2025-12-26 14:18:39 +0000

    User commit

--- src/main/java/com/example/demo/service/UserAccountServiceImpl.java
+++ src/main/java/com/example/demo/service/UserAccountServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.entity.enums.RoleType;
 import com.example.demo.dto.AuthRequest;
 import com.example.demo.dto.AuthResponse;
 import com.example.demo.dto.RegisterRequest;
